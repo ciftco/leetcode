3b997c8ce50b14bd00366bdf6ab574967b77722a
Oguz <oguzciftci@gmail.com>
2022-04-23 20:46:44 +0300

    mid

---
+++ src/main/java/com/ciftci/leetcode/easy/S28_ImplementstrStr.java
@@ -0,0 +1,12 @@
+package com.ciftci.leetcode.easy;
+
+public class S28_ImplementstrStr {
+
+	public static void main(String[] args) {
+
+	}
+
+	public int strStr(String haystack, String needle) {
+		return 0;
+	}
+}
